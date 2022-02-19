a1218f89460a38161873f6bfd9ab8431d61fddb9
Ed Summers <ehs@pobox.com>
2022-02-19 12:14:43 -0500

    some new images

--- screens/install.sh
+++ screens/install.sh
@@ -3,9 +3,16 @@
 IMAGES="$HOME/remcustom/screens/images"
 
 # Pick some images from the images directory.
+# angelus-novus.jpg
+# arrival.png
+# building-shapes.png
+# ferns.jpg
+# paglen-shape-of-clouds.png
+# timeless-way-of-building1.png
+# tree.jpg
 
-POWEROFF="$IMAGES/arrival.png"
-SUSPENDED="$IMAGES/building-shapes.png"
+POWEROFF="$IMAGES/tree.jpg"
+SUSPENDED="$IMAGES/ferns.jpg"
 
 cp $POWEROFF /usr/share/remarkable/poweroff.png
 cp $SUSPENDED /usr/share/remarkable/suspended.png
